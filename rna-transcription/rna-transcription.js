251921c496fbc9c4f127511203c839fc8dd550fb
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-08-31 17:43:16 -0600

    finish rna-transcription

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,1 +1,31 @@
+var rnaTranscriber = function() {};
 
+rnaTranscriber.prototype.toRna = function(input) {
+  var dna = input.split('');
+  var rna = [];
+
+
+  for(var i = 0; i < dna.length; i++) {
+    switch(dna[i]) {
+      case 'G':
+        rna.push('C');
+        continue;
+      case 'C':
+        rna.push('G');
+        continue;
+      case 'A':
+        rna.push('U');
+        continue;
+      case 'T':
+        rna.push('A');
+        continue;
+      default:
+        throw ('Invalid input')
+        break;
+    }
+  }
+
+  return rna.join('')
+}
+
+module.exports = rnaTranscriber;
